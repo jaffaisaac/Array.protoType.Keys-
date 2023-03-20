df19c2adeff2aaf78f7c1816ffe0d0b69f4f2171
jaffa Isaac <jaffaisaac4@gmail.com>
2023-03-20 15:28:06 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,9 @@
+// Array.prototype.keys()  this return s keys of each index in an array
+const name =['joy','may','ben','bob']
+
+const keysOnly = name.keys()
+
+// using for-of loop 
+for(keys of keysOnly){
+    console.log(keys)
+}
